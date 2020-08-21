017510488543d0348026cf1229b233a1d637d5bc
saullocastro <saullogiovani@gmail.com>
2020-08-21 07:55:12 +0200

    TST: fixed theta function

--- tests/test_linear_buckling_FW_VAT_P_VT.py
+++ tests/test_linear_buckling_FW_VAT_P_VT.py
@@ -12,7 +12,25 @@ from bfsccylinder import (BFSCCylinder, update_KC0, update_KG, DOF, DOUBLE, INT,
 KC0_SPARSE_SIZE, KG_SPARSE_SIZE)
 from bfsccylinder.quadrature import get_points_weights
 from bfsccylinder.utils import assign_constant_ABD
-from bfsccylinder.vatfunctions import theta_VAT_P_x
+
+
+def theta_VAT_P_x(x, L, desvar):
+    theta_VP_1, theta_VP_2, theta_VP_3 = desvar
+    x1 = 0
+    x2 = L/4
+    x3 = L/2
+    x4 = 3*L/4
+    x5 = L
+    if x <= L/2:
+        N1 = (x - x2)*(x - x3)/((x1 - x2)*(x1 - x3))
+        N2 = (x - x1)*(x - x3)/((x2 - x1)*(x2 - x3))
+        N3L = (x - x1)*(x - x2)/((x3 - x1)*(x3 - x2))
+        return N1*theta_VP_1 + N2*theta_VP_2 + N3L*theta_VP_3
+    else:
+        N3R = (x - x4)*(x - x5)/((x3 - x4)*(x3 - x5))
+        N4 = (x - x3)*(x - x5)/((x4 - x3)*(x4 - x5))
+        N5 = (x - x3)*(x - x4)/((x5 - x3)*(x5 - x4))
+        return N3R*theta_VP_3 + N4*theta_VP_2 + N5*theta_VP_1
 
 
 def test_linear_buckling(plot=False):
@@ -97,8 +115,8 @@ def test_linear_buckling(plot=False):
             xi = points[i]
             xlocal = x1 + (x2 - x1)*(xi + 1)/2
             assert xlocal > x1 and xlocal < x2
-            theta_local = theta_VAT_P_x(xlocal, L, theta_VP_1, theta_VP_2,
-                    theta_VP_3)
+            desvar = theta_VP_1, theta_VP_2, theta_VP_3
+            theta_local = theta_VAT_P_x(xlocal, L, desvar)
             steering_angle = abs(theta_min - theta_local)
             plyt_local = plyt/np.cos(np.deg2rad(steering_angle))
             lam = read_stack(stack=[theta_local, -theta_local],
