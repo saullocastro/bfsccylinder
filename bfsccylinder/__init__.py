c397eaa0c2ddc76838d2590a18a6aa54a3f2b533
saullocastro <saullogiovani@gmail.com>
2020-08-21 23:07:52 +0200

    DEV: KG with constant membrane stress

--- bfsccylinder/__init__.py
+++ bfsccylinder/__init__.py
@@ -4,7 +4,8 @@ BFSCCYLINDER - Implementation of the BFSC cylinder finite element
 Author: Saullo G. P. Castro
 
 """
-from .bfsccylinder import BFSCCylinder, update_KC0, update_KG, update_M
+from .bfsccylinder import (BFSCCylinder, update_KC0, update_KG,
+        update_KG_constant_stress, update_M)
 from .bfsccylinder import INT, DOUBLE, KC0_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
 DOF = 10
 
