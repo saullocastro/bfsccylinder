07ee7a237ff35537450f628e90c1cd448e4ec8d1
Saullo G. P. Castro <saullogiovani@gmail.com>
2023-04-12 12:34:24 +0200

    BUG: fixed int long long long issue

--- bfsccylinder/__init__.py
+++ bfsccylinder/__init__.py
@@ -4,8 +4,14 @@ kinematics
 
 
 """
+import numpy as np
+
 from .bfsccylinder import (BFSCCylinder, update_KC0, update_KCNL, update_KG,
         update_KG_constant_stress, update_M, update_fint)
-from .bfsccylinder import INT, DOUBLE, KC0_SPARSE_SIZE, KCNL_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
+from .bfsccylinder import KC0_SPARSE_SIZE, KCNL_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
+
+
+INT = int
+DOUBLE = np.float64
 DOF = 10
 
