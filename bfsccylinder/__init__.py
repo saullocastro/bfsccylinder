78079ef790bb17152b4e4feadf08b2428e0fba3f
Saullo G. P. Castro <saullogiovani@gmail.com>
2024-09-25 14:50:07 +0200

    BUG: fixing int number compatibility

--- bfsccylinder/__init__.py
+++ bfsccylinder/__init__.py
@@ -4,14 +4,23 @@ kinematics
 
 
 """
+import ctypes
+
 import numpy as np
+
 
 from .bfsccylinder import (BFSCCylinder, update_KC0, update_KCNL, update_KG,
         update_KG_constant_stress, update_M, update_fint)
 from .bfsccylinder import KC0_SPARSE_SIZE, KCNL_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
 
 
-INT = int
+if ctypes.sizeof(ctypes.c_long) == 8:
+    # here the C long will correspond to np.int64
+    INT = np.int64
+else:
+    # here the C long will correspond to np.int32
+    INT = np.int32
+
 DOUBLE = np.float64
 DOF = 10
 
